8c50033e4b109de1972e7809d8a0fb4acb7486e6
Charleu <sdlimeira0@gmail.com>
2024-09-05 17:52:55 -0300

    implememtando as propriedades get e set completas na classe Produto

---
+++ DescontoProduto/Produto.cs
@@ -0,0 +1,44 @@
+﻿namespace DescontoProduto
+{
+    class Produto
+    {
+        private string _nome;
+        private double _precoOriginal;
+        private int _quantidade;
+
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        public double PrecoOriginal
+        {
+            get { return _precoOriginal; }
+            set { _precoOriginal = value; }
+        }
+
+        public int Quantidade
+        {
+            get { return _quantidade; }
+            set { _quantidade = value; }
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            PrecoOriginal = preco;
+            Quantidade = quantidade;
+        }
+
+        public double CalcularPrecoTotal(double desconto)
+        {
+            return PrecoOriginal * Quantidade * (1 - desconto);
+        }
+
+        public double CalcularDesconto(double precoOriginalTotal, double precoTotal)
+        {
+            return precoOriginalTotal - precoTotal;
+        }
+    }
+}
---
+++ DescontoProduto/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DescontoProduto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sep = new string('-', 40);
+            Console.WriteLine("Informe o nome do produto:");
+            string nomeProduto = Console.ReadLine();
+            Console.WriteLine("Informe o preço do produto:");
+            double precoProduto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a quantidade do produto:");
+            int quantidadeProduto = int.Parse(Console.ReadLine());
+
+            double desconto = CalcularDesconto(quantidadeProduto);
+
+            Produto produto = new Produto(nomeProduto, precoProduto, quantidadeProduto);
+
+            double precoTotal = produto.CalcularPrecoTotal(desconto);
+            double valorDesconto = produto.CalcularDesconto(precoProduto * quantidadeProduto, precoTotal);
+
+            Console.WriteLine(sep);
+            Console.WriteLine($"Valor total: {precoTotal.ToString("C")}");
+            Console.WriteLine(sep);
+            Console.WriteLine($"Desconto: {valorDesconto.ToString("C")}");
+            Console.WriteLine(sep);
+        }
+
+        static double CalcularDesconto(int quantidade)
+        {
+            if (quantidade > 50)
+                return 0.25;
+            if (quantidade > 20)
+                return 0.20;
+            if (quantidade > 10)
+                return 0.10;
+            return 0;
+        }
+    }
+}
+
